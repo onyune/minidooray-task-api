741a27ec12cd402eca028502d959415bfad81235
onyune <wjsdbwls0303@gmail.com>
2026-05-14 09:21:11 +0900

    firtscommit

---
+++ src/main/java/com/nhnacademy/minidoorayteam9taskapi/MinidoorayTeam9TaskApiApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidoorayteam9taskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinidoorayTeam9TaskApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinidoorayTeam9TaskApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/minidoorayteam9taskapi/MinidoorayTeam9TaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidoorayteam9taskapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinidoorayTeam9TaskApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
